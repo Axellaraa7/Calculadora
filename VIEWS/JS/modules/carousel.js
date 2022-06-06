536c9531ac6bd78c94a1f393401436d06749d9b4
Axel Lara <m.axellaraa@gmail.com>
2022-06-06 16:07:47 -0500

    A: more json to rqst w ftch

--- VIEWS/JS/modules/carousel.js
+++ VIEWS/JS/modules/carousel.js
@@ -1,8 +1,8 @@
 const d = document;
 const urls = [
   "./ASSETS/DATA/cientificos.json",
-  "./ASSETS/DATA/cientificos.json",
-  "./ASSETS/DATA/cientificos.json"
+  "./ASSETS/DATA/ciencias.json",
+  "./ASSETS/DATA/lenguajes.json"
 ];
 
 export function loadCarousels (){
@@ -10,30 +10,63 @@ export function loadCarousels (){
   figures.forEach( async (figure,index) => {
     let response = await fetch(urls[index]),
     data = await response.json();
-    timingCarousel(figure, data);
+    timingCarousel(figure, data.data);
   });
 
 }
 
 const timingCarousel = (figure,data) => {
-  let cont = 0;
-  const interval = setInterval(()=>{
-    let link = figure.children[0],
-      img = link.children[0],
-      figcaption = link.children[1];
-    
-    link.classList.add("fadeIn");
-    setTimeout(()=>{
-      // console.log("TimeOut");
-      img.setAttribute("alt",data.cientificos[cont].name);
-      figcaption.textContent = data.cientificos[cont].name;
-      link.setAttribute("href",data.cientificos[cont].bio);
-      img.setAttribute("src",data.cientificos[cont].img);
-      link.classList.remove("fadeIn");
-      cont++;
-      if(cont >= data.cientificos.length) cont = 0;
-    },400);
-  },5000);
+  let cont = 0, 
+  link = figure.children[0],
+  img = link.children[0],
+  figcaption = link.children[1];
 
+  img.setAttribute("src",data[cont].img);
+  img.setAttribute("alt",data[cont].name);
+  figcaption.textContent = data[cont].name;
+  link.setAttribute("href",data[cont].bio);
+  link.classList.remove("fadeIn");
+
+  const interval = setInterval(()=>{ 
+    link.classList.add("fadeIn");
+    setTimeout( () => {
+      // console.log("TimeOut");
+      cont++;
+      if(cont >= data.length) cont = 0;
+      img.setAttribute("src",data[cont].img);
+      img.setAttribute("alt",data[cont].name);
+      figcaption.textContent = data[cont].name;
+      link.setAttribute("href",data[cont].bio);
+      link.classList.remove("fadeIn");
+    },400);
+  },4000);
   //clearInterval(interval);
 }
+
+/* const domElements = [
+    {
+      domElement: img,
+      attrs: ["src","alt"],
+      datas: [
+        data.cientificos[cont].img,
+        data.cientificos[cont].name
+      ]
+    },{
+      domElement: figcaption,
+      attrs: ["textContent"],
+      datas: [data.cientificos[cont].name]
+    },{
+      domElement: link,
+      attrs: ["href"],
+      datas: [data.cientificos[cont].bio]
+    }
+  ]  */
+/* const setAttributes = (domElementsObj) =>{
+  domElementsObj.forEach(element =>{
+    let { domElement, attrs, cont, datas } = element;
+    attrs.forEach( (attr,index) =>{
+      if(attr === "textContent") domElement.textContent = datas;
+      else domElement.setAttribute(attr,datas[index]);
+    })
+  })
+} */
